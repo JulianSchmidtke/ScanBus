560e700a3ff8d16d865b965cdb2cf58f540311b2
Julian Schmidtke <julian.schmidtke@hsw-stud.de>
2023-09-23 08:33:44 +0000

    Async Calculations

--- Services/Function.cs
+++ Services/Function.cs
@@ -1,38 +1,33 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Numerics;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using ScanBus.Models;
 using ScanBus.Service;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
 
 namespace ScanBus.Function;
 public class Function
 {
     private readonly ILogger<Function> _logger;
 
-    private static List<BusNotice> busNotices = new();
-    private static List<ProcessedNotice> processedNotices = new();
+    private static List<ProcessedNotice> notices = new();
 
     public Function(ILogger<Function> log)
     {
         _logger = log;
     }
 
-            
+
     [FunctionName("PostBusNotice")]
     [OpenApiOperation(operationId: "Run", tags: new[] { "ScanBus" })]
     [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(BusNotice), Description = "BusNotice", Required = true)]
@@ -44,63 +39,72 @@ public class Function
 
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
         var busNotice = JsonConvert.DeserializeObject<BusNotice>(requestBody);
-        busNotices.Add(busNotice);
 
-        _ = Task.Run(async () => await processNotice(busNotice));
-        
+        ProcessedNotice processedNotice = new()
+        {
+            Id = notices.Count(),
+            Base64Image = busNotice.Base64Image,
+            Longitude = busNotice.Longitude,
+            Latitude = busNotice.Latitude,
+            Driver = busNotice.Driver,
+            BusId = busNotice.BusId
+        };
+        notices.Add(processedNotice);
+
+        _ = Task.Run(async () => await ProcessNotice(processedNotice));
+
         return new OkResult();
     }
 
     [FunctionName("GetProcessedNotices")]
     [OpenApiOperation(operationId: "Run", tags: new[] { "ScanBus" })]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(ProcessedNotice), Description = "The OK response")]
-    public async Task<IActionResult> GetProcessedNotices(
+    public IActionResult GetProcessedNotices(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "processedNotices")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        
-        return new OkObjectResult(processedNotices);
+
+        return new OkObjectResult(notices);
     }
 
-    [FunctionName("GetBusNotices")]
+    [FunctionName("GetProcessedNotice")]
     [OpenApiOperation(operationId: "Run", tags: new[] { "ScanBus" })]
-    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(BusNotice), Description = "The OK response")]
-    public async Task<IActionResult> GetBusNotices(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "busNotices")] HttpRequest req)
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(ProcessedNotice), Description = "The OK response")]
+    public IActionResult GetProcessedNotice(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "processedNotice/{id}")] HttpRequest req, int id)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        
-        return new OkObjectResult(busNotices);
+
+        var notice = notices.Where(n => n.Id == id).FirstOrDefault();
+
+        return new OkObjectResult(notice);
     }
 
-
-    private async Task processNotice(BusNotice busNotice)
+    private async Task ProcessNotice(ProcessedNotice notice)
     {
-
-        PredictionHelper predictionHelper = new();
-        var predictions = predictionHelper.PredictImage(busNotice.Base64Image);
-
-        (var carImage, var licensePlateImage) = PredictionHelper.GetCroppedImages(busNotice.Base64Image, predictions);
-        
-        Address address = await CoordService.GetAddress(busNotice.Longitude, busNotice.Latitude);
-        
-        //Todo: OCR
-        var licensePlateText = OCRHelper.AnalyzeImage(licensePlateImage);
-        //Todo: Auto
-
-        ProcessedNotice processedNotice = new()
-        {
-            Address = address,
-            Base64Image = busNotice.Base64Image,
-            Base64ImageCar = carImage,
-            Base64ImageLicensePlate = licensePlateImage,
-            Longitude = busNotice.Longitude,
-            Latitude = busNotice.Latitude,
-            Driver = busNotice.Driver,
-            BusId = busNotice.BusId
-        };
-        processedNotices.Add(processedNotice);
+        await ProcessGeoInformation(notice);
+        await ProcessImage(notice);
     }
 
+    private async Task ProcessGeoInformation(ProcessedNotice notice)
+    {
+        Thread.Sleep(2000);
+        Address address = await CoordService.GetAddress(notice.Longitude, notice.Latitude);
+        notice.Address = address;
+    }
 
+    private async Task ProcessImage(ProcessedNotice notice)
+    {
+        PredictionHelper predictionHelper = new();
+        var predictions = predictionHelper.PredictImage(notice.Base64Image);
+
+        Thread.Sleep(5000);
+        (var carImage, var licensePlateImage) = PredictionHelper.GetCroppedImages(notice.Base64Image, predictions);
+        notice.Base64ImageCar = carImage;
+        notice.Base64ImageLicensePlate = licensePlateImage;
+
+        Thread.Sleep(9000);
+        string licensePlateText = await OCRHelper.AnalyzeImage(licensePlateImage);
+        notice.LicensePlate = licensePlateText;
+    }
 }
--- Services/ProcessedNotice.cs
+++ Services/ProcessedNotice.cs
@@ -4,14 +4,15 @@ using ScanBus.Models;
 namespace ScanBus.Function;
 public class ProcessedNotice
 {
-
+    public int Id { get; set; }
+    public string BusId { get; set; }
+    public string Driver { get; set; }
+    public Address Address { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public string LicensePlate { get; set; }
     public string Base64Image { get; set; }
     public string Base64ImageCar { get; set; }
     public string Base64ImageLicensePlate { get; set; }
     public string ImageContentType { get; set; }
-    public Address Address { get; set; }
-    public string Latitude { get; set; }
-    public string Longitude { get; set; }
-    public string Driver { get; set; }
-    public string BusId { get; set; }
 }
