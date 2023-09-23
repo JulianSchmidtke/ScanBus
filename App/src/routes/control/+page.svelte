5443e096181c4bc5fefde76d0d708c39795c9f53
David Scheidt <david.scheidt@humotion.net>
2023-09-23 13:42:42 +0200

    Add map to control page

---
+++ App/src/routes/control/+page.svelte
@@ -0,0 +1,20 @@
+<iframe
+	width="1194"
+	height="350"
+	src="https://www.openstreetmap.org/export/embed.html?bbox=7.634629011154176%2C51.9515259713887%2C7.641710042953492%2C51.95491474004837&amp;layer=mapnik&amp;marker=51.95322%2C7.6381699999999455"
+	style="border: 1px solid black"
+/>
+<main>
+	<div>
+		<h1>Hello World</h1>
+	</div>
+</main>
+
+<style>
+	div {
+		margin: 0 auto;
+		background: white;
+		margin-top: -100px;
+		width: 40%;
+	}
+</style>
